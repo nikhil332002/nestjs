fa96defea78e8619e8ffe933f8356e1297f9c734
Nikhil R <nikhil2002raghu@gmail.com>
2024-10-22 21:09:48 +0530

    setup of global validation

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+
+    @Get()
+    listMessages(){
+        return "hello"
+    }
+
+    @Post()
+    createMessages(@Body() body:any){
+        console.log(body)
+        return body;
+    }
+
+    @Get('/:id')
+    getMessage(@Param('id') id:string){
+        console.log(id);
+        return id;      
+    }
+}

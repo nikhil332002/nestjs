59e145ef36b24c91f4661c8882293568818136e1
Nikhil R <nikhil2002raghu@gmail.com>
2024-10-23 17:31:05 +0530

    added controller

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
+import { ReportsController } from './reports.controller';
 
 @Module({
   controllers: [ReportsController],
